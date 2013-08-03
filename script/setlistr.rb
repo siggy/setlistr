3b8acc6b886300a6d1245cf48c87c26fa0e44cfc
Andrew Seigner <andrews@twitter.com>
2013-08-03 10:29:47 -0700

    parse tweet json

--- script/setlistr.rb
+++ script/setlistr.rb
@@ -1,17 +1,20 @@
+require 'time'
 require 'yaml'
+require 'json'
 require 'active_support'
 
 def normalize(str)
   ActiveSupport::Multibyte::Chars.new(str).normalize(:kd).downcase.to_s
 end
 
-songs = YAML::load(File.open(File.join(File.dirname(__FILE__), "../config/phish_songs.yml"))).map do |s|
-  normalize(s.to_s)
+songs = YAML::load(File.open(File.join(File.dirname(__FILE__), "../config/phish_songs.yml"))).map do |song|
+  normalize(song.to_s)
 end
 puts "#{songs.count} songs loaded"
 
-tweets = YAML::load(File.open(File.join(File.dirname(__FILE__), "../config/phish_stream.yml"))).map do |t|
-  [t[0], normalize(t[1])]
+tweets = []
+File.open(File.join(File.dirname(__FILE__), "../config/phish_tweets.json")).each do |line|
+  tweets << JSON.parse(line)
 end
 puts "#{tweets.count} tweets loaded"
 
@@ -20,11 +23,11 @@ song_hash = Hash[escaped_songs.map {|v| [v,[]]}]
 song_regex = /#{escaped_songs.join('|')}/
 
 tweets.each do |tweet|
-  next if !(song_regex === tweet[1])
+  next if !(song_regex === tweet['text'])
 
   escaped_songs.each do |song|
-    if tweet[1] =~ /#{song}/
-      song_hash[song] << tweet[0]
+    if tweet['text'] =~ /#{song}/
+      song_hash[song] << Time.parse(tweet['created_at']).to_i
     end
   end
 end
